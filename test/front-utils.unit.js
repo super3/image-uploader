782425905f37cb8bb6ab5152abbfd3e499050c9c
Shawn Wilkinson <me@super3.org>
2016-10-12 12:23:02 -0400

    Increase Coverage on Front Utils

--- test/front-utils.unit.js
+++ test/front-utils.unit.js
@@ -3,4 +3,17 @@
 var expect = require('chai').expect;
 var utils = require('../public/javascripts/utils.js');
 
-console.log(expect, utils);
+describe('utils', function() {
+
+  describe('#humanFileSize', function() {
+    it('check 1000 bytes', function() {
+      expect(utils.humanFileSize(1000, true)).to.equal("1.0 kB");
+    });
+
+    it('check 1024 bytes', function() {
+      expect(utils.humanFileSize(1024, false)).to.equal("1.0 KiB");
+    });
+
+  });
+
+});

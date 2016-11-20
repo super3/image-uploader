c69a4df3f656e273f5d51ef2bb3dc0092dd3399b
Shawn Wilkinson <me@super3.org>
2016-11-20 17:14:03 -0500

    Remove Delete Stuff

--- public/javascripts/drop.js
+++ public/javascripts/drop.js
@@ -4,48 +4,21 @@
 Dropzone.options.newThread = {
   init: function() {
     var self = this;
-    // config
-    self.options.addRemoveLinks = true;
-    self.options.dictRemoveFile = 'Delete';
 
-    // bind events
-
-    //New file added
+    // new file added
     self.on('addedfile', function(file) {
       console.log('new file added ', file);
     });
 
-    // Send file starts
+    // send file starts
     self.on('sending', function(file) {
       console.log('upload started', file);
-      $('.meter').show();
     });
 
-    // File upload Progress
-    self.on('totaluploadprogress', function(progress) {
-      console.log('progress ', progress);
-      $('.roller').width(progress + '%');
-    });
-
-    self.on('queuecomplete', function(progress) {
-      $('.meter').delay(999).slideUp(999);
-    });
-
+    // file completed
     self.on('complete', function(file) {
       dom.addThreadDom(file.xhr.response, file);
       console.log('complete upload');
-    });
-
-    // On removing file
-    self.on('removedfile', function(file) {
-      console.log(file);
-      $.ajax({
-        url: '/uploaded/files/' + file.name,
-        type: 'DELETE',
-        success: function(result) {
-          console.log(result);
-        }
-      });
     });
 
   }

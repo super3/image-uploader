9686432e80be3571260e8177ce5f2c71058786e3
Shawn Wilkinson <me@super3.org>
2016-11-20 17:33:09 -0500

    Clean up Upload.js

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -3,26 +3,16 @@
 
 $('#newThread').on('submit', function(){
 
-  var files = $('#upload-input', this).get(0).files;
-  var title = $("[name='title']", this);
-  var comment = $("[name='comment']", this);
+  // grab form data
+  var title = $('[name=\'title\']', this);
+  var comment = $('[name=\'comment\']', this);
 
   console.log(title.val());
-  console.log(comment.val());
-
-  if (files.length > 0){
+  // make sure we don't have an empty title
+  if (title.val().length > 0){
     // create a FormData object which will be sent as the data payload
-    // in the AJAX request
+    // in the AJAX request, with the comment and title fields
     var formData = new FormData();
-
-    // loop through all the selected files and add them to the formData object
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-
-      // add the files to formData object for the data payload
-      formData.append('uploads[]', file, file.name);
-    }
-
     formData.append('comment', comment.val());
     formData.append('title', title.val());
 
@@ -32,38 +22,8 @@ $('#newThread').on('submit', function(){
       data: formData,
       processData: false,
       contentType: false,
-      success: function(data){
-          dom.addThreadDom(data, files);
-      },
-      xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
-
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
-
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%');
-            $('.progress-bar').width(percentComplete + '%');
-
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done');
-            }
-
-          }
-
-        }, false);
-
-        return xhr;
-      }
+      success: function(data){ console.log(data); }
     });
-
   }
 
   return false;

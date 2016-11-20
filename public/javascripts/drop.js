eb574235965f18c4dd237751130859e5c7e8d42d
Shawn Wilkinson <me@super3.org>
2016-11-20 17:03:05 -0500

    JSHint Fixes

--- public/javascripts/drop.js
+++ public/javascripts/drop.js
@@ -1,41 +1,43 @@
+'use strict';
+/*globals $:false */
+
 Dropzone.options.newThread = {
   init: function() {
     var self = this;
     // config
     self.options.addRemoveLinks = true;
-    self.options.dictRemoveFile = "Delete";
+    self.options.dictRemoveFile = 'Delete';
 
     // bind events
 
     //New file added
-    self.on("addedfile", function(file) {
+    self.on('addedfile', function(file) {
       console.log('new file added ', file);
     });
 
     // Send file starts
-    self.on("sending", function(file) {
+    self.on('sending', function(file) {
       console.log('upload started', file);
       $('.meter').show();
     });
 
     // File upload Progress
-    self.on("totaluploadprogress", function(progress) {
-      console.log("progress ", progress);
+    self.on('totaluploadprogress', function(progress) {
+      console.log('progress ', progress);
       $('.roller').width(progress + '%');
     });
 
-    self.on("queuecomplete", function(progress) {
+    self.on('queuecomplete', function(progress) {
       $('.meter').delay(999).slideUp(999);
     });
 
-    self.on("complete", function(file) {
-      console.log(file);
+    self.on('complete', function(file) {
       dom.addThreadDom(file.xhr.response, file);
       console.log('complete upload');
     });
 
     // On removing file
-    self.on("removedfile", function(file) {
+    self.on('removedfile', function(file) {
       console.log(file);
       $.ajax({
         url: '/uploaded/files/' + file.name,
--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,4 +1,5 @@
 'use strict';
+/*globals $:false */
 
 $('#newThread').on('submit', function(){
 

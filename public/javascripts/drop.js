b7187aae49c03846f838d8c1698cad83f9ad46c8
Shawn Wilkinson <me@super3.org>
2016-11-22 14:33:11 -0500

    Fixed Some Linting Errors

--- public/javascripts/drop.js
+++ public/javascripts/drop.js
@@ -6,17 +6,17 @@ Dropzone.options.newThread = {
   // The configuration
   autoProcessQueue: false,
   uploadMultiple: true,
-  parallelUploads: 100,
-  maxFiles: 100,
-  previewsContainer: ".dropzone-previews",
-  clickable: ".dropzone-upload",
+  parallelUploads: 10,
+  maxFiles: 50,
+  previewsContainer: '.dropzone-previews',
+  clickable: '.dropzone-upload',
 
   // The setting up of the dropzone
   init: function() {
     var self = this;
 
     // First change the button to actually tell Dropzone to process the queue.
-    this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
+    this.element.querySelector('button[type=submit]').addEventListener('click', function(e) {
       // Make sure that the form isn't actually being sent.
       e.preventDefault();
       e.stopPropagation();
@@ -25,15 +25,15 @@ Dropzone.options.newThread = {
 
     // Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
     // of the sending event because uploadMultiple is set to true.
-    this.on("sendingmultiple", function() {
+    this.on('sendingmultiple', function() {
       // Gets triggered when the form is actually being sent.
       // Hide the success button or the complete form.
     });
-    this.on("successmultiple", function(files, response) {
+    this.on('successmultiple', function(files, response) {
       // Gets triggered when the files have successfully been sent.
       // Redirect user or notify of success.
     });
-    this.on("errormultiple", function(files, response) {
+    this.on('errormultiple', function(files, response) {
       // Gets triggered when there was an error sending the files.
       // Maybe show form again, and notify user of error
     });
